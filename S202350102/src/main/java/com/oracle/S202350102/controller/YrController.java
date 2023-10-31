aa748435d25e9e8499d1ae2182c0fc44cdba8dad
Yu-Rim Seok <syr96@naver.com>
2023-10-31 19:58:55 +0900

    yr test push

--- S202350102/src/main/java/com/oracle/S202350102/controller/YrController.java
+++ S202350102/src/main/java/com/oracle/S202350102/controller/YrController.java
@@ -10,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class YrController {
+	// test
+	
 	@RequestMapping(value = "checkBoard")
 	public String checkBoard() {
 		System.out.println("YrController index Start...");
