95f449c23edebff7aaa909a50ca7d6b43a1b15a5
jkp0330 <144098613+jkp0330@users.noreply.github.com>
2023-10-31 19:53:32 +0900

    first commit

--- S202350102/src/main/java/com/oracle/S202350102/controller/JkController.java
+++ S202350102/src/main/java/com/oracle/S202350102/controller/JkController.java
@@ -1,6 +1,7 @@
 package com.oracle.S202350102.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -8,8 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
-@RequiredArgsConstructor
-public class JkController {
 
+public class JkController {
+	@RequestMapping(value="/mypage")
+	public String writeFormCommunity(Integer user_num, Model model ) {
+		System.out.println("JkController mypage Start... ");
+		
+		return "mypage";
 
 }
+	}
