f7916204e2972d4604ae1f8fcf7b245fa9137dea
bigwise92 <bigwise92@naver.com>
2023-11-21 12:43:01 +0900

    태현 - 결제 진행중

--- S202350102/src/main/java/com/oracle/S202350102/service/thService/ThOrder1Service.java
+++ S202350102/src/main/java/com/oracle/S202350102/service/thService/ThOrder1Service.java
@@ -1,7 +1,9 @@
 package com.oracle.S202350102.service.thService;
 
+import com.oracle.S202350102.dto.Order1;
+
 public interface ThOrder1Service {
 
-	int 			insertOrder1(int user_num, Object kakaoSucInfo);
+	int 			insertOrder(Order1 order1);
 
 }
